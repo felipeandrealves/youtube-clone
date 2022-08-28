929802a9320562c8c024d90e24f77e27360cd43b
Felipe Andre Alves <felipe_alves_sbo@hotmail.com>
2022-08-28 15:45:29 -0300

    refactor: set file root aplication

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Youtube Clone</title>
+      </Head>
+
+      <div>
+        <p>Home</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
